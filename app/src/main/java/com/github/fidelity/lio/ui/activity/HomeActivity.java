c6714ef485d57ceed996e995a6b665e7aae7645a
Maurício Pimenta <mjbpimenta@gmail.com>
2016-09-24 22:04:02 -0300

    Trying configure maps

--- app/src/main/java/com/github/fidelity/lio/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/fidelity/lio/ui/activity/HomeActivity.java
@@ -1,9 +1,8 @@
 package com.github.fidelity.lio.ui.activity;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -63,16 +62,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -101,8 +94,8 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             fragment = new AcceptanceNetworkFragment();
         }
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
+        getSupportFragmentManager()
+                .beginTransaction()
                 .replace(R.id.content_frame, fragment)
                 .commit();
 
--- app/src/main/java/com/github/fidelity/lio/ui/fragment/AcceptanceNetworkFragment.java
+++ app/src/main/java/com/github/fidelity/lio/ui/fragment/AcceptanceNetworkFragment.java
@@ -1,69 +1,47 @@
 package com.github.fidelity.lio.ui.fragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.github.fidelity.lio.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AcceptanceNetworkFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AcceptanceNetworkFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AcceptanceNetworkFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AcceptanceNetworkFragment extends Fragment implements OnMapReadyCallback {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    @BindView(R.id.mapView)
+    MapView mapView;
 
     public AcceptanceNetworkFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InformationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AcceptanceNetworkFragment newInstance(String param1, String param2) {
-        AcceptanceNetworkFragment fragment = new AcceptanceNetworkFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static AcceptanceNetworkFragment newInstance() {
+        return new AcceptanceNetworkFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_acceptance_network, container, false);
+        ButterKnife.bind(this, view);
+        mapView.getMapAsync(this);
+        return view;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_information, container, false);
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 }
